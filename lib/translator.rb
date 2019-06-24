8a25e0da874dddd991cbbe2a0e68bdcd1033ffc8
Samantha Kane <s.kane133@gmail.com>
2019-06-24 03:23:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,6 +25,11 @@ def get_japanese_emoticon(yaml_file, emoticon)
 #   binding.pry
 # else 
 #   return lib["get_meaning"][emoticon]
+ if library["get_emoticon"].include?(english_emoticon) 
+    library["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 end
 
